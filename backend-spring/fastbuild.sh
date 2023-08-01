0ae619fbd5b5c0a88a2686ffca6c5319d09694ed
feedbaek <nicek789@naver.com>
2023-08-01 23:44:35 +0900

    Feat: fastbuild.sh 강제 git pull

--- backend-spring/fastbuild.sh
+++ backend-spring/fastbuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 git checkout $GIT_BRANCH
-git pull
+git fetch --all
+git reset --hard origin/$GIT_BRANCH
+git pull origin $GIT_BRANCH
 
 ./gradlew build -x test
 java -jar -Dspring.profiles.active=prod build/libs/UnderTheC-Backend-0.0.1-SNAPSHOT.jar
--- frontend-react/fastbuild.sh
+++ frontend-react/fastbuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 git checkout $GIT_BRANCH
-git pull
+git fetch --all
+git reset --hard origin/$GIT_BRANCH
+git pull origin $GIT_BRANCH
 
 npm install
 npm start
